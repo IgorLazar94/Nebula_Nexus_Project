88f8e5bb43f9b4e0a817deedfdb0f9dd6105f0d6
IgorLazar <81099964+IgorLazar94@users.noreply.github.com>
2023-07-04 13:02:49 +0300

    create product manager script, products and mine prefabs

--- Assets/Scripts/Builds/Spawner.cs
+++ Assets/Scripts/Builds/Spawner.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class Spawner : GenericBuild, IProduce
 {
+    [SerializeField] private TypeOfProduct typeOfProduct;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private float spawnerProduceTimer;
+    private GameObject productPrefab;
 
+    private void Start()
+    {
+        productPrefab = ProductManager.Instance.ChooseProductPrefab(typeOfProduct);
+        StartCoroutine(SpawnIron());
+    }
 
     public void ProduceProduct()
     {
-        
+
+    }
+
+    private IEnumerator SpawnIron()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnerProduceTimer);
+
+        }
     }
 }
