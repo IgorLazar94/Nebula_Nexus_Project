207cb6d00e747270b5da5e7065407e08a1410ee1
IgorLazar <81099964+IgorLazar94@users.noreply.github.com>
2023-07-04 22:48:22 +0300

    modified factory logic

--- Assets/Scripts/Builds/Factory.cs
+++ Assets/Scripts/Builds/Factory.cs
@@ -1,14 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Factory : GenericBuild, IProduce, IReceive
 {
-    [SerializeField] private GameObject spawnPoint;
+    private TypeOfProduct typeOfProductReceive;
+    private TypeOfProduct typeOfProductProduce;
+
+    private GameObject producePrefab;
+    private GameObject receivePrefab;
+
+    private List<Product> ironsList = new List<Product>();
+    private List<Product> swordsList = new List<Product>();
+
+
+
+    //[SerializeField] private GameObject spawnPoint;
     [SerializeField] private GameObject receivePoint;
+
 
     private int ironOnFactory = 0;
     private int swordsOnFactory = 0;
+
+    // Iron
+    private float offsetXIron;
+    private float offsetYIron;
+    private float offsetZIron;
+
+    private int widthLimitIron = 4;
+    private int lengthLimitIron = 2;
+
+    private float lengthIron = 0;
+    private float heightIron = 0;
+    private float widthIron = 0;
+
+
+    private void Start()
+    {
+        receivePrefab = ProductManager.Instance.ChooseProductPrefab(typeOfProductReceive);
+        producePrefab = ProductManager.Instance.ChooseProductPrefab(typeOfProductProduce);
+
+    }
+
+    private void AddReceiveProduct(Vector3 _spawnPos)
+    {
+        Debug.Log(_spawnPos + " spawn pos for iron");
+        GameObject productObject = Instantiate(receivePrefab, _spawnPos, receivePrefab.transform.rotation);
+        Product product = productObject.gameObject.GetComponent<Product>();
+        ironsList.Add(product);
+    }
+
+    private void CalculateNewPosition()
+    {
+        lengthIron++;
+        if (lengthIron > widthLimitIron)
+        {
+            lengthIron = 0;
+            heightIron++;
+            if (heightIron > lengthLimitIron)
+            {
+                lengthIron = 0;
+                heightIron = 0;
+                widthIron++;
+            }
+        }
+    }
 
     public void ProduceProduct(Vector3 setProductPos)
     {
@@ -18,7 +75,11 @@ public class Factory : GenericBuild, IProduce, IReceive
     public void ReceiveProduct(int productAmount)
     {
         SetIronToFactory(productAmount);
-        SetPositionToIron();
+
+        for (int i = 0; i < productAmount; i++)
+        {
+            PlaceNewIronAtReceivePos();
+        }
     }
 
     private void SetIronToFactory(int ironAmount)
@@ -26,14 +87,17 @@ public class Factory : GenericBuild, IProduce, IReceive
         ironOnFactory += ironAmount;
     }
 
-    private void SetPositionToIron()
+    private void PlaceNewIronAtReceivePos()
     {
-
+        CalculateNewPosition();
+        Vector3 spawnPosition = receivePoint.transform.position + new Vector3(lengthIron * offsetXIron, widthIron * offsetYIron, -(heightIron * offsetZIron));
+        AddReceiveProduct(spawnPosition);
+        // iron in sword coroutine
     }
 
 
-    private void Update()
-    {
-        Debug.Log(ironOnFactory + " ironOnFactory");
-    }
+    //private void Update()
+    //{
+    //    Debug.Log(ironOnFactory + " ironOnFactory");
+    //}
 }
